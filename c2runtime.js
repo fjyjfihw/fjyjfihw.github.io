03819e6de60f7d21ba942b57edd29054c779db56
fjyjfihw <fjyjfihw@sharklasers.com>
2016-09-26 22:59:07 +0800

    CCC
    CCC

--- c2runtime.js
+++ c2runtime.js
@@ -20202,10 +20202,10 @@ cr.behaviors.solid = function(runtime)
 }());
 cr.getObjectRefTable = function () { return [
 	cr.plugins_.Keyboard,
+	cr.plugins_.TiledBg,
+	cr.plugins_.Touch,
 	cr.plugins_.Sprite,
 	cr.plugins_.Text,
-	cr.plugins_.Touch,
-	cr.plugins_.TiledBg,
 	cr.behaviors.solid,
 	cr.behaviors.Platform,
 	cr.behaviors.scrollto,
